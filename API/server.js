e9a3026b6fe13df1dc1b0b9d1fb234ef11245918
JPetsis <snowblind777@gmail.com>
2020-10-18 01:22:49 +0200

    first commit

---
+++ API/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!!</h1>")
+})
+
+app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
