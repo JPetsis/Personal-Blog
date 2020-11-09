2fed56af33f0166c66675db9940e84879d904131
JPetsis <snowblind777@gmail.com>
2020-11-09 16:30:52 +0100

    init create react app and install material ui

---
+++ Client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css"
+import CssBaseline from "@material-ui/core/CssBaseline"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        Learn React
+      </header>
+    </div>
+  )
+}
+
+export default App
